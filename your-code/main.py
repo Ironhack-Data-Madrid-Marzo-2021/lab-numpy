4590974fd9a452609a31f5a44441f1d2a2a5b285
asiokfd <asiokfd@LAPTOP-T2VJVASV.localdomain>
2021-03-30 20:27:00 +0200

    a falta del bonus

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,76 @@
 #1. Import the NUMPY package under the name np.
+import numpy as np
 
 
 
 #2. Print the NUMPY version and the configuration.
 
-
+np.version.version
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
+a=np.random.randint(0,100,size=(2, 3, 5)) 
+#a=np.random.rand(2,3,5)
+# la tercera no la encuentro o me dice que está obsoleta en el jupyter
 
 #4. Print a.
 
-
+print (a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b= np.ones ((5, 2, 3))
+#b= np.ones (2 ,3), dtype=int)
 
 
 #6. Print b.
-
+print (b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+a.shape==b.shape
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#no, porque tienen distinto tamaño
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c= np.reshape (b, (2,3,5))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+#ahora funciona porque tienen el mismo tamaño
+d=a+c 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print (a)
+print (d)
 
+# a cada elemento, o punto en el mapa, de a se le suma su elemento correspondiente en c, la diferencia sería:
 
-
+ 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e=a*c
 
 #13. Does e equal to a? Why or why not?
 
-
+#son iguales porque n+1=1, si los valores de c fueran distintos, el resultado sería para cada valor de a se le multiplica el de c
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max= np.amax (a)
+d_min= np.min (a)
+d_mean= np.mean (a)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f= np.empty (2,3,5)
 
 
 """
@@ -74,8 +82,12 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
+f[(d>j) & (d<k)]= (25)
+f[(d>k) & (d<l)]= (75)
+f[d==k]=(50)
+f[d==l]=(100)
+f[d==j]=(0)
+# no lo consigo sin numpy
 
 
 """
@@ -99,7 +111,7 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
-
+print (f)
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
@@ -111,4 +123,4 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'D',  'D',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
-"""
+"""# intentandolo de momento...
