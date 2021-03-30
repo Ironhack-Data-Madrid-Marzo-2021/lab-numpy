5b53235a4db9f92afd712942cef812177e81db1e
as-Solo <asdelsolo@gmail.com>
2021-03-30 20:05:46 +0200

    ...

--- your-code/main.py
+++ your-code/main.py
@@ -1,67 +1,73 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+print (np.__version__)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+aa = (np.random.randn(2, 3, 5,)*10).round(1)
+a = np.random.randint(low= 0, high = 10, size=(2, 3, 5))
+aaa = (np.random.rand(2, 3, 5)).round(1)
 
 
 #4. Print a.
-
+print (a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5, 2, 3))
 
 
 #6. Print b.
-
+print (b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+print (a.shape == b.shape)
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+# np.add(a, b)       error--------> operands could not be broadcast together with shapes (2,3,5) (5,2,3)
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = np.transpose(b, axes = (1,2,0))
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = np.add(a, c)
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+print (a)
+print(d)
 
 
 
 #12. Multiply a and c. Assign the result to e.
-
-
+print ('comparamos estas dos')
+#e = a*c         me lo hace, pero no me da el mismo resultado de datos
+e = np.add(a,c)
 
 #13. Does e equal to a? Why or why not?
-
-
+print (a)  # es igual, ya que al ser b una matriz de unos y por lo tanto c también, multiplicar un número por si mismo da el número en sí mismo
+print (b)
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f = np.empty((2, 3, 5))
 
 
 
@@ -74,6 +80,12 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+
+f[(d > d_min) & (d < d_mean)] = 25
+f[(d > d_mean) & (d < d_max)] = 75
+f[d == d_mean] = 50
+f[d == d_min] = 0
+f[d == d_max] = 100
 
 
 
@@ -98,7 +110,9 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print (d)
+print (f)
+# Sí, he obtenido el resultado esperado
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +126,24 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+g = np.empty((2, 3, 5))
+
+
+g = f.astype(str)
+
+for x in range (len(g)):
+        for y in range (len(g[x])):
+                for z in range (len(g[x][y])):
+                        if g[x][y][z] == '0.0':
+                                g[x][y][z] = 'A'
+                        elif g[x][y][z] == '25.0':
+                                g[x][y][z] = 'B'
+                        elif g[x][y][z] == '50.0':
+                                g[x][y][z] = 'C'
+                        elif g[x][y][z] == '75.0':
+                                g[x][y][z] = 'D'
+                        elif g[x][y][z] == '100.0':
+                                g[x][y][z] = 'E'
+
+print (g)
