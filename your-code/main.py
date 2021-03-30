328f8d5785b330be05563e608943e759c99b05d2
“John <johndoe@example.comclear>
2021-03-30 17:04:56 +0200

    Falta bonus

--- your-code/main.py
+++ your-code/main.py
@@ -6,65 +6,90 @@ import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-np.version.version
+print(f"Numpy version:\n  {np.version.version}")
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+import random 
 
+a = np.random.randint(0, 9, (2, 3, 5))
+#print(a)
+
+a2 = np.random.rand(2, 3, 5)
+#print(a2)
+
+a3 = np.random.random_sample((2, 3, 5))
+#print(a3)
 
 #4. Print a.
 
-
+print(f"La matriz d:\n  {a}")
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5, 2, 3))
 
 #6. Print b.
 
-
+print(f"La matriz d:\n  {b}")
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+print(f"No, la matriz a {a.shape} tiene los valores inversos de la matriz b {b.shape}")
 
 
 
 #8. Are you able to add a and b? Why or why not?
 
+print("No porque no tienen la misma forma.")
+
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.transpose(b).reshape(2, 3, 5)
+print(f"La matriz c:\n  {c.shape}")
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
+print(f"La matriz d:\n  {d}")
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
-
+print(f"La matriz d:\n  {a}")
+print(f"La matriz d:\n  {d}")
+print(f"La matriz b está formada por valores de tipo float {type(b[(1,1,1)])}")
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
+print(f"La matriz e:\n {e}")
 
 #13. Does e equal to a? Why or why not?
 
+print("Los valores de a y e son iguales")
+print(a == e)
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d).round(2)
 
+print(f"El valor máximo de d \n {d} \n  es: {d_max}")
+print(f"El valor mínimo de d \n {d} \n  es: {d_min}")
+print(f"El valor medio de d \n {d} \n  es: {d_mean}")
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty((2, 3, 5), dtype=int)
+print(f"La matriz e:\n {f}")
 
 
 """
